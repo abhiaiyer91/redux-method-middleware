f2ce5b868b7190cad71bd56ee1f5e37faf089d25
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-05-19 09:56:23 -0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-export const sum = (a, b) => {
-  return new Promise(resolve => resolve(a + b));
-};
+/**
+ * A redux middleware for processing Meteor methods
+ * When an action is dispatched, it will pass through our middleware.
+ * if denoted a method, we will dispatch the action with readyState of loading
+ * The method passed in is then called, and dispatches further ready states for success/error
+ * The reducer shape should include { data, readyState } for use in the UI
+ * @returns {Function}
+ */
+export default function methodMiddleware() {
+  return (next) => {
+    return (action) => {
+      const { method, ...rest } = action;
+      if (!method) {
+        return next(action);
+      }
+      next({
+        ...rest,
+        readyState: 'loading'
+      });
+      return method((error, result) => {
+        if (error) {
+          return next({
+            error: error.reason,
+            readyState: 'error', ...rest
+          });
+        }
+        return next({
+          data: result,
+          readyState: 'success', ...rest
+        });
+      });
+    };
+  };
+}
